07a37b275b3e9b382b8867f4c3e3f438923c8121
Michael Olsen <molsen@Michaels-MacBook-Air.local>
2020-01-28 20:52:00 -0500

    phase two

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
 const userTable = document.querySelector('#users')
+const pageBar = document.querySelector('#pageBar')
 
 fetch('https://acme-users-api-rev.herokuapp.com/api/users')
     .then( response => response.json())
-    .then( result => renderUsers(result.users))
-
+    .then( result => {
+        renderUsers(result.users)
+        pager(result.count)
+    })
 
 const renderUsers = (users) => {
+    const header = `      
+    <tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Email</th>
+    <th>Title</th>
+    </tr>
+    `
     const html = users.map( user => {
         return `
-        <tr> <td> ${user.fullName} </td> </tr>`
+        <tr> 
+        <td> ${user.firstName} </td> 
+        <td> ${user.lastName} </td> 
+        <td> ${user.email} </td> 
+        <td> ${user.title} </td> 
+        </tr>
+
+        `
     }).join('')
 
-    userTable.innerHTML = html
+    userTable.innerHTML = `${header}${html}`
+}
+
+const pager = (count) => {
+    pages = count / 50
+    let pagesArray = []
+    for (let i = 1; i < pages; i++) {
+        pagesArray.push(i)
+    }
+    console.log(pagesArray)
+
+    const html = pagesArray.map( page => {
+        return `
+        <a href='${page}'>`
+    } )
+    pageBar.innerHTML = html
 }
